e9055b64201feea78c5ff52f32405051b53e91b4
Lance McConnell <lance.mcconnell@gmail.com>
2022-08-19 02:54:32 -0500

    first commit

---
+++ src/main/java/com/summer/hackathon/controller/ApplicationController.java
@@ -0,0 +1,17 @@
+package com.summer.hackathon.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApplicationController {
+    
+    @GetMapping("/")
+    public String getGreeting(){
+
+        return "Welcome to Summer Hackathon 2022";
+        
+    }
+}
